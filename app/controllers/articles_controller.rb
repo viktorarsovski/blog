27e7886e8d3f4d5033d887b1918f206e6cec95d4
Viktor <macedonian.wow@live.com>
2020-05-09 20:09:47 +0200

    Adding new feature: update Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,10 +14,24 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    if article.save
+    if @article.save
       redirect_to @article
     else
       render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
     end
   end
 
@@ -27,3 +41,4 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :body)
   end
 end
+
