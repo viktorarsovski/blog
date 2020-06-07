6acb37ba42226a7843ac14f91a3ed8d1218b7c78
Viktor <macedonian.wow@live.com>
2020-06-07 14:35:53 +0200

    Comments Controller and View implementation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,23 @@ class CommentsController < ApplicationController
     redirect_to comment.article
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+
+    @article = @comment.article
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @article = @comment.article
+
+    if @comment.update(comment_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
   private
 
   def comment_params
