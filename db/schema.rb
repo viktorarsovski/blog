296c9833fdcf0726ea42a2878fb4746b7335f271
Viktor <macedonian.wow@live.com>
2020-06-18 18:06:06 +0200

    New Users Controller with :new, :create and :show actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_164427) do
+ActiveRecord::Schema.define(version: 2020_06_13_154131) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2020_06_09_164427) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "comments", "articles"
