c1f48fb70cb71c6d69faf0a6ba0bc8d0ac54982c
Viktor <macedonian.wow@live.com>
2020-06-21 18:57:03 +0200

    New Users Controller with :new, :create and :show actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:session][:password])
       session[:user_id] = user.id
+      
 
     else
       flash.now[:danger] = 'Email and password miss match'
